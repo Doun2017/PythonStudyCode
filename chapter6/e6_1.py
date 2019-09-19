e1b3793f427c8a949450d109c61e217582f04d2f
liushi_work <liushi2008wh@163.com>
2019-09-19 21:33:42 +0800

    chapter6

--- chapter6/e6_1.py
+++ chapter6/e6_1.py
@@ -38,4 +38,44 @@ for name in names:
         print("Invite "+name+" to vote!")
 
 
+#6-7
+print("#6-7")
+people1 = {"first_name": "yan",
+"last_name": "ren",
+"age": 16,
+"city ": "shanghai",}
+people2 = {"first_name": "hang",
+"last_name": "shu",
+"age": 15,
+"city ": "shanghai",}
+persons = []
+persons.append(people)
+persons.append(people1)
+persons.append(people2)
+for item in persons:
+    print(item)
 
+
+#6-8
+print("#6-8")
+pet_dog = {"type": "dog",
+"master": "liushi",}
+pet_cat = {"type": "cat",
+"master": "renyan",}
+pet_pig = {"type": "pig",
+"master": "shuhang",}
+pets = []
+pets.append(pet_dog)
+pets.append(pet_cat)
+pets.append(pet_pig)
+for item in pets:
+    print(item)
+
+
+#6-9
+print("#6-9")
+favorite_places = {"liushi":["shanghai", "beijing", "guangzhou"],
+"renyan":["shanghai", "beijing", "guangzhou"],
+"xialijie":["shanghai", "beijing", "guangzhou"],
+}
+print(favorite_places)
