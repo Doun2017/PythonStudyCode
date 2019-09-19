d7f615e4a6f0bd01d265df8e7f7001a7506966e8
liushi_work <liushi2008wh@163.com>
2019-09-19 11:51:59 +0800

    4.3

---
+++ chapter4/e4_5.py
@@ -0,0 +1,26 @@
+#4-5
+million = list(range(1, 1000001))
+print(min(million))
+print(max(million))
+print(sum(million))
+
+#4-6
+odd_numbers = list(range(1,20,2))
+for i in odd_numbers:
+    print(i)
+
+#4-7
+print("#4-7")
+odd_numbers = list(range(3,31,3))
+for i in odd_numbers:
+    print(i)
+#4-8
+print("#4-8")
+ten_numbers = list(range(1,11))
+for i in ten_numbers:
+    print(i**3)
+#4-9
+print("#4-9")
+ten_numbers = [i**3 for i in range(1,11)]
+print(ten_numbers)
+
