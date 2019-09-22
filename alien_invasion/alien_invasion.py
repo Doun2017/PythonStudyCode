9102b1712fc2dcab3c424cae08306fd68e07eefd
Doun <liushi2008wh@163.com>
2019-09-22 22:33:48 +0800

    chapter 13

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,7 +1,10 @@
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
+from alien import Alien
+from game_stats import GameStats
 import game_functions as gf
 
 def run_game():
@@ -12,16 +15,23 @@ def run_game():
 	(ai_settings.screen_width,ai_settings.screen_height))
 	# 设置标题
 	pygame.display.set_caption("Alien Invasion")
+	stats = GameStats(ai_settings)
 	# 创建一艘飞船
 	ship = Ship(ai_settings, screen)
-	
+	# 创建一个用于存储子弹的编组
+	bullets = Group()
+	aliens = Group()
+	# 创建外星人群
+	gf.create_fleet(ai_settings, screen, ship, aliens)
+
 	# 开始游戏的主循环
 	while True:
-		gf.check_events(ship)
-		ship.update()
-		gf.update_screen(ai_settings, screen, ship)
+		gf.check_events(ai_settings, screen, ship, bullets)
+		if stats.game_active:
+			ship.update()
+			gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+			gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+		gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
-		# 让最近绘制的屏幕可见
-		pygame.display.flip()
-	
+
 run_game()
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -7,3 +7,15 @@ class Settings():
 		self.screen_height = 800
 		self.ship_speed_factor = 1.5
 		self.bg_color = (135, 206, 235)
+		self.ship_limit = 3
+		# 子弹设置
+		self.bullet_speed_factor = 3
+		self.bullet_width = 3
+		self.bullet_height = 15
+		self.bullet_color = 60, 60, 60
+		self.bullets_allowed = 3
+		# 外星人设置
+		self.alien_speed_factor = 1
+		self.fleet_drop_speed = 10
+		# fleet_direction 为 1 表示向右移，为 -1 表示向左移
+		self.fleet_direction = 1
