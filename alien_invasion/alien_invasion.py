9340d936fa6117e47b45cc593bcf42fe030bf8d6
Doun <liushi2008wh@163.com>
2019-09-22 23:49:07 +0800

    14.2

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -5,6 +5,7 @@ from settings import Settings
 from ship import Ship
 from alien import Alien
 from game_stats import GameStats
+from button import Button
 import game_functions as gf
 
 def run_game():
@@ -16,6 +17,8 @@ def run_game():
 	# 设置标题
 	pygame.display.set_caption("Alien Invasion")
 	stats = GameStats(ai_settings)
+	# 创建 Play 按钮
+	play_button = Button(ai_settings, screen, "Play")
 	# 创建一艘飞船
 	ship = Ship(ai_settings, screen)
 	# 创建一个用于存储子弹的编组
@@ -26,12 +29,13 @@ def run_game():
 
 	# 开始游戏的主循环
 	while True:
-		gf.check_events(ai_settings, screen, ship, bullets)
+		gf.check_events(ai_settings, screen, stats, play_button, ship,
+			aliens, bullets)
 		if stats.game_active:
 			ship.update()
 			gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
 			gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
-		gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+		gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
 
 
 run_game()
