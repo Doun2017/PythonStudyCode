a2af0848ea6e2353a6ff618fd65acb576496e0b9
Doun <liushi2008wh@163.com>
2019-09-25 22:53:04 +0800

    Signed-off-by: Doun <liushi2008wh@163.com>

--- chapter15/e15_1.py
+++ chapter15/e15_1.py
@@ -4,9 +4,9 @@ x_values = list(range(1, 5001))
 y_values = [x**3 for x in x_values]
 plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=40)
 # 设置图表标题并给坐标轴加上标签
-plt.title("Square Numbers", fontsize=24)
+plt.title("Cube Numbers", fontsize=24)
 plt.xlabel("Value", fontsize=14)
-plt.ylabel("Square of Value", fontsize=14)
+plt.ylabel("Cube of Value", fontsize=14)
 # 设置刻度标记的大小
 plt.tick_params(axis='both', which='major', labelsize=14)
 # 设置每个坐标轴的取值范围
